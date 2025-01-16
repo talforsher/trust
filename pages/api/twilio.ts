440cded75ed334071f5278573f7d409cfa2c2d6b
talforsher <talforsher1@gmail.com>
2025-01-16 23:07:57 +0200

    Update Twilio API response to decode text correctly for Cloudinary media URL
    - Modified the decoding logic for the text parameter to include the correct text in the Cloudinary media URL.
    - Added a console log for incoming request body to assist with debugging.
    - Ensured existing functionality remains intact while improving media presentation accuracy.

--- pages/api/twilio.ts
+++ pages/api/twilio.ts
@@ -34,7 +34,7 @@ const formatTwilioResponse = async (text: string) => {
   try {
     const twiml = new twilio.twiml.MessagingResponse();
     const message = twiml.message(text);
-    const decodedText = decodeURIComponent("text");
+    const decodedText = decodeURIComponent("text text");
     message.media(
       `https://res.cloudinary.com/efsi/image/upload/b_gray,co_rgb:FFFFFF,l_text:Arial_100:${decodedText},r_10,o_76,g_south,y_40/xrjx758eqm8zb9ctuet1.jpg`
     );
@@ -78,6 +78,7 @@ export default async function handler(
     // }
 
     const incomingMsg = req.body.Body?.trim();
+    console.log(req.body);
     const from = req.body.From;
 
     // Validate required fields
